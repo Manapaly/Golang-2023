4b853cc519d2a44feab8d2ba18b40a59ed447186
Manapaly <ernat3505@gmail.com>
2023-04-11 15:42:12 +0600

    sdfsd

--- assignment_3j/initializers/connectDB.go
+++ assignment_3j/initializers/connectDB.go
@@ -3,14 +3,13 @@ package initializers
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"os"
 )
 
 var DB *gorm.DB
 var err error
 
 func ConnectDatabase() {
-	DB, err = gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open("host=db user=postgres password=321qaz dbname=BookStore port=5432"), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to DATABASE")
